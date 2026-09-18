77d46e1c253288630093da81c50773e0fc474d5f
Najil <najiljan26@gmail.com>
2026-09-18 10:13:15 +0530

    Made the weather auto update

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ setInterval(() => {
     updateTime()
 }, 1000);
 
+setInterval(() => {
+    updateWeather()
+}, 10 * 60 * 1000);
+
 function updateTime() {
     let timeP = document.getElementById("top_nav_bar_time")
     let currentTime = new Date().toLocaleString()
